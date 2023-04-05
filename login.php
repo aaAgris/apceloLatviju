877d666a69ae0304646eb6d2ef4a92072d56be2f
AnnijaZ <annija.zorgenberga@gmail.com>
2023-04-05 09:51:28 +0300

    Login sakums

---
+++ login.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ielogošanās sistēmā</title>
+    <link rel="stylesheet" href="../files/style_login.css">
+</head>
+<body>
+	<div class="container" id="container">
+		<div class="form-container sign-in-container">
+
+
+
+
+<?php
+	if(isset($_POST["Ielogoties"])){
+		require "../files/connect_db.php";
+
+		session_start();
+
+		$Lietotajvards = mysqli_real_escape_string($savienojums, 
+		$_POST["Lietotajvards"]);
+
+		$Parole = mysqli_real_escape_string($savienojums, 
+		$_POST["Parole"]);
+
+		$sqlVaicajums = "SELECT * FROM ielogoties WHERE
+		Lietotajvards = '$Lietotajvards'";
+
+		$rezultats = mysqli_query($savienojums, $sqlVaicajums);
+
+		if(mysqli_num_rows($rezultats) == 1){
+			while($row = mysqli_fetch_array($rezultats)){
+				if(password_verify($Parole, $row["Parole"])){
+					$_SESSION["Lietotajvards"] = $Lietotajvards;
+					header("location:index.php");
+				}else{
+					echo "<p class='p2'>Nepareizs lietotajvards vai parole!</p>";
+				}
+			}
+		}else{
+			echo "<p class='p2'>Nepareizs lietotajvards vai parole!</p>";
+		}
+
+	}
+
+	if(isset($GET['logout'])){
+		session_destroy();
+	}
+	
+?>
+
+
+
+
+			<form action="" method="POST">
+				<img class="logo" src="../images/lvt.png">
+				<h1>Ielogoties sistēmā</h1>
+				<input type="email" name="Lietotajvards" placeholder="Lietotājvārds" />
+				<input type="password" name="Parole" placeholder="Parole" />
+				<!--<input type="submit" name="Ielogoties" placeholder="Ielogoties"/>-->
+				<button name="Ielogoties">Ielogoties</button>
+			</form>
+		</div>
+		<div class="overlay-container">
+			<div class="overlay">
+				<div class="overlay-panel overlay-right">
+					<h1>Esi sveicināts!</h1>
+					<p>Audzēkņu uzņemšanas sistēmas administrēšanas vietne paredzēta tikai skolas vadībai!</p>
+					<button class="ghost" id="signUp">Doties uz galveno lapu</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
